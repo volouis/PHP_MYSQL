110180c242f57c2d159cefac3a2f4bbe31a573b5
louisqvo <louisqvo@gmail.com>
2019-01-29 19:40:23 -0500

    starting new php project

---
+++ public/staff/subjects/index.php
@@ -0,0 +1,13 @@
+<?php require('../../../private/initialize.php'); ?>
+
+<?php $page_title = 'Subjects'; ?>
+<?php include(SHARED_PATH . '/staff_header.php'); ?>
+
+<div id="content">
+
+
+
+</div>
+
+
+<?php include(SHARED_PATH . '/staff_footer.php'); ?>
